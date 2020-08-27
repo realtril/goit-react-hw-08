fa5695a7109cde5afa9f9c2bf4899950bf45823e
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-08-27 15:17:17 +0300

    Updates

---
+++ precache-manifest.2fd21bfda4f26b29a364e8863dd13422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14aaaf445326665ef5719341d8597279",
+    "url": "/goit-react-hw-08/index.html"
+  },
+  {
+    "revision": "63a3698e804803f06b31",
+    "url": "/goit-react-hw-08/static/css/main.70ae0ac3.chunk.css"
+  },
+  {
+    "revision": "afec392db31cfe5eb1a9",
+    "url": "/goit-react-hw-08/static/js/2.5b6bb67e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08/static/js/2.5b6bb67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a3698e804803f06b31",
+    "url": "/goit-react-hw-08/static/js/main.a4b8e180.chunk.js"
+  },
+  {
+    "revision": "77faba62a561b4097df1",
+    "url": "/goit-react-hw-08/static/js/runtime-main.54b77a02.js"
+  }
+]);
